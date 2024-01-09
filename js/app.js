72413fab5393cb7ea5efea12bea6eb42d634a702
sagaputra <sagaputra8@gmail.com>
2024-01-09 23:39:45 +0700

    prevent plagiarism and fix thumbnail

--- js/app.js
+++ js/app.js
@@ -1092,3 +1092,21 @@ function openLink(platform) {
     window.open("https://www.instagram.com/saga_putraa");
   }
 }
+// prevent plagiarism
+document.onkeydown = (e) => {
+  if (e.key == 123) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "I") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "C") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "J") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == "U") {
+    e.preventDefault();
+  }
+};
